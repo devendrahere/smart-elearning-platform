c8b359aa594626ba7c196b358b658a570f820ba2
Devendra C <devendrac2323@gmail.com>
2025-10-06 18:05:10 +0530

    initialized  and day one setup

---
+++ src/main/java/com/edusmart/EduSmartApplication.java
@@ -0,0 +1,13 @@
+package com.edusmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduSmartApplication.class, args);
+		System.out.println("hello");
+	}
+}
